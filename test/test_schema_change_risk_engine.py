8789dce9a9a99a41f903291ced73be5f8d27fc73
David Murphy <david.b.murphy.tx@gmail.com>
2023-03-16 15:28:17 +0000

    fix(flake8): workflow testing

--- test/test_schema_change_risk_engine.py
+++ test/test_schema_change_risk_engine.py
@@ -3,9 +3,9 @@ import sys
 
 import pytest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from schema_change_risk_engine.schema_change_risk_engine import SchemaChangeRiskEngine
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 @pytest.fixture
@@ -29,15 +29,15 @@ def test_rule_no_rename(engine, columns):
     success3, error_msg3 = engine.rule_no_rename(stmt3, columns)
     success4, error_msg4 = engine.rule_no_rename(stmt4, columns)
     success5, error_msg5 = engine.rule_no_rename(stmt5, columns)
-    assert success1 == False
+    assert success1 is False
     assert "Renaming tables is not allowed" in error_msg1
-    assert success2 == False
+    assert success2 is False
     assert "Renaming columns is not allowed" in error_msg2
-    assert success3 == True
+    assert success3 is True
     assert error_msg3 is None
-    assert success4 == True
+    assert success4 is True
     assert error_msg4 is None
-    assert success5 == True
+    assert success5 is True
     assert error_msg5 is None
 
 
@@ -48,11 +48,11 @@ def test_rule_no_primary_key(engine, columns):
     success1, error_msg1 = engine.rule_no_primary_key(stmt1, columns)
     success2, error_msg2 = engine.rule_no_primary_key(stmt2, columns)
     success3, error_msg3 = engine.rule_no_primary_key(stmt3, columns)
-    assert success1 == False
+    assert success1 is False
     assert "Table must have a primary key" in error_msg1
-    assert success2 == True
+    assert success2 is True
     assert error_msg2 is None
-    assert success3 == True
+    assert success3 is True
     assert error_msg3 is None
 
 
@@ -61,9 +61,9 @@ def test_rule_no_datetime(engine, columns):
     stmt2 = "CREATE TABLE my_table (id INT, created_at TIMESTAMP);"
     success1, error_msg1 = engine.rule_no_datetime(stmt1, columns)
     success2, error_msg2 = engine.rule_no_datetime(stmt2, columns)
-    assert success1 == False
+    assert success1 is False
     assert "DATETIME data type is not allowed" in error_msg1
-    assert success2 == True
+    assert success2 is True
     assert error_msg2 is None
 
 
@@ -72,9 +72,9 @@ def test_rule_no_foreign_keys(engine, columns):
     stmt2 = "ALTER TABLE my_table ADD COLUMN column1 INT;"
     success1, error_msg1 = engine.rule_no_foreign_keys(stmt1, columns)
     success2, error_msg2 = engine.rule_no_foreign_keys(stmt2, columns)
-    assert success1 == False
+    assert success1 is False
     assert "Foreign keys are not allowed" in error_msg1
-    assert success2 == True
+    assert success2 is True
     assert error_msg2 is None
 
 
@@ -83,9 +83,9 @@ def test_rule_no_triggers(engine, columns):
     stmt2 = "CREATE TABLE my_table (id INT);"
     success1, error_msg1 = engine.rule_no_triggers(stmt1, columns)
     success2, error_msg2 = engine.rule_no_triggers(stmt2, columns)
-    assert success1 == False
+    assert success1 is False
     assert "Triggers are not allowed" in error_msg1
-    assert success2 == True
+    assert success2 is True
     assert error_msg2 is None
 
 
@@ -97,11 +97,11 @@ def test_rule_no_triggers(engine, columns):
 #     success1, error_msg1 = engine.rule_no_redundant_indexing(stmt1, columns)
 #     success2, error_msg2 = engine.rule_no_redundant_indexing(stmt2, columns)
 #     success3, error_msg3 = engine.rule_no_redundant_indexing(stmt3, columns)
-#     assert success1 == True
+#     assert success1 is True
 #     assert error_msg1 is None
-#     assert success2 == True
+#     assert success2 is True
 #     assert error_msg2 is None
-#     assert success3 == False
+#     assert success3 is False
 #     assert "Redundant indexing is not allowed" in error_msg3
 
 
@@ -112,11 +112,11 @@ def test_rule_no_blob_or_text(engine, columns):
     success1, error_msg1 = engine.rule_no_blob_or_text(stmt1, columns)
     success2, error_msg2 = engine.rule_no_blob_or_text(stmt2, columns)
     success3, error_msg3 = engine.rule_no_blob_or_text(stmt3, columns)
-    assert success1 == False
+    assert success1 is False
     assert "BLOB columns are not allowed" in error_msg1
-    assert success2 == False
+    assert success2 is False
     assert "TEXT columns are not allowed" in error_msg2
-    assert success3 == True
+    assert success3 is True
     assert error_msg3 is None
 
 
@@ -127,9 +127,9 @@ def test_rule_no_enum_or_set(engine, columns):
     success1, error_msg1 = engine.rule_no_enum_or_set(stmt1, columns)
     success2, error_msg2 = engine.rule_no_enum_or_set(stmt2, columns)
     success3, error_msg3 = engine.rule_no_enum_or_set(stmt3, columns)
-    assert success1 == False
+    assert success1 is False
     assert "ENUM data type is not allowed" in error_msg1
-    assert success2 == False
+    assert success2 is False
     assert "SET data type is not allowed" in error_msg2
-    assert success3 == True
+    assert success3 is True
     assert error_msg3 is None
